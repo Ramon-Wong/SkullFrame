9c13447b4a11cd2863fa3ea14a3b6baed202fef8
Ramon Wong <rcnwong78@gmail.com>
2024-06-29 11:05:20 -0300

    removed the bulky looking function with promises and uses callback instead

--- web/script/index.js
+++ web/script/index.js
@@ -1,17 +1,27 @@
 document.addEventListener('DOMContentLoaded', () => { 
-    document.getElementById('alertButton').addEventListener('click', () => { 
-        console.log(callCHelloWorld());
-    });
+	document.getElementById('alertButton').addEventListener('click', () => { 
+		// console.log(callCHelloWorld());
+		ThisIsAFunction(["What is one plus one"], function(result) { console.log("Received from C: " + result);});
+	});
 });
 
 
-function callCHelloWorld() {
-    return new Promise((resolve, reject) => {
-        window.webkit.messageHandlers.js_Call.postMessage(["Hello from JavaScript", 42, true]);
+function onCFunctionReturn(result){
+	if(typeof window.onCFunctionReturn ===  'function'){
+		window.onCFunctionCallback(result);
+	}
+}
 
-        window.onCFunctionReturn = function(result) {
-            console.log(result);
-            resolve(result);
-        };
-    });
+function ThisIsAFunction(message, callback) {
+	window.onCFunctionCallback = callback;
+	window.webkit.messageHandlers.js_Call.postMessage(message);
+}
+
+window.onCFunctionReturn = onCFunctionReturn;
+
+
+function testFunction() {
+	ThisIsAFunction(["What is one plus one"], function(result) {
+		console.log("Received from C: " + result);
+	});
 }
